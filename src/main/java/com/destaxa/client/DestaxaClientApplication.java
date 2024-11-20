a030a557de6fefdbb8d6e3a86317701d424f0d49
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-11-20 15:35:14 -0300

    cria o projeto com o springboot

---
+++ src/main/java/com/destaxa/client/DestaxaClientApplication.java
@@ -0,0 +1,13 @@
+package com.destaxa.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestaxaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestaxaClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/destaxa/client/DestaxaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destaxa.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestaxaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
